b22947cd121b066183c721ccd00e9f9819d7d0cf
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-12-20 20:30:14 +0700

    Membuat Modul Baru dan Component Navigasi

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -2,8 +2,18 @@
     <div>
         <router-link :to="{ name: 'login'}">Login</router-link>
         <router-link :to="{ name: 'register'}">Register</router-link>
+        <Navigation />
         <main class="py-4">
             <router-view></router-view>
         </main>
     </div>
 </template>
+
+<script>
+import Navigation from "./Navigation"
+export default {
+    components: {
+        Navigation
+    }
+}
+</script>
